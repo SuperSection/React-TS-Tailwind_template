2638ce6bbc4238f9d93e4cc173f2ff882a8480a8
SuperSection <soumosarkar297@gmail.com>
2024-03-06 13:53:23 +0530

    control the View Reply option

--- src/components/CommentSection/index.tsx
+++ src/components/CommentSection/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 // Interface for a single comment
 interface Comment {
   id: number;
@@ -6,17 +9,30 @@ interface Comment {
 }
 
 const Comment: React.FC<Comment> = ({ id, message, replies }) => {
+  const [showReply, toggleReply] = useState(false);
+
   // Render the comment and optionally render replies recursively
   return (
     <div className="p-3 m-3 flex flex-col border border-black rounded-lg">
       <div key={id} className="flex justify-between items-center">
         <p>{message}</p>
+
         <div className="flex text-blue-500 font-medium gap-4">
-          <span className="cursor-pointer">View Reply</span>
+          // if there're replies (in a Comment or a subComment) then "View Reply"
+          {replies?.length !== 0 && (
+            <span
+              onClick={() => toggleReply(!showReply)}
+              className="cursor-pointer"
+            >
+              View Reply
+            </span>
+          )}
           <span className="cursor-pointer">Add Reply</span>
         </div>
       </div>
-      {replies &&
+
+      {showReply &&
+        replies &&
         replies.map((reply) => (
           <Comment key={reply.id} {...reply} /> // Pass all props to nested Comment components
         ))}
